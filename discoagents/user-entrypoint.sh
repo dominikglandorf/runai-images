2fa19ac170fe41f095ff56a0299ba91697b2dbb0
Dominik Glandorf <do.gl@gmx.de>
2025-12-09 21:02:51 +0100

    remove venv

--- discoagents/user-entrypoint.sh
+++ discoagents/user-entrypoint.sh
@@ -75,8 +75,14 @@ fi
 
 echo "USER_HOME: $USER_HOME"
 
-gosu ${GASPAR_USER} python -m venv /home/$GASPAR_USER/.venv --system-site-packages
-gosu ${GASPAR_USER} /home/$GASPAR_USER/.venv/bin/pip install -r $USER_HOME/disco-agents/requirements.txt
-gosu ${GASPAR_USER} /home/$GASPAR_USER/.venv/bin/python -m ipykernel install --name ".venv" --user
+
+#gosu ${GASPAR_USER} python -m venv /home/$GASPAR_USER/.venv --system-site-packages
+#gosu ${GASPAR_USER} /home/$GASPAR_USER/.venv/bin/pip install -r $USER_HOME/disco-agents/requirements.txt
+REQ="$USER_HOME/disco-agents/requirements.txt"
+
+if [ -f "$REQ" ]; then
+    pip install -r "$REQ"
+fi
+#gosu ${GASPAR_USER} /home/$GASPAR_USER/.venv/bin/python -m ipykernel install --name ".venv" --user
 
 exec gosu ${GASPAR_USER} /bin/bash -c "$*"
